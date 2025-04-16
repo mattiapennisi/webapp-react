239314f1e403726892ff69df0bc6dede91b97dc0
mattiapennisi <mattiapennisi0@gmail.com>
2025-04-16 16:51:49 +0200

    Refactor HomePage and MovieDetails components to enhance layout and structure

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,24 @@
 export default function HomePage() {
     return (
         <>
-            <h1 className="text-center">Homepage</h1>
+            <div className="container">
+                <h1 className="text-center mb-5">Homepage</h1>
+
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-6 g-3">
+                    {
+
+                    }
+                    <div className="col">
+                        <div class="card p-3">
+                            <img class="card-img-top" src="" alt="Title" />
+                            <div class="card-body">
+                                <h4 class="card-title">Movie</h4>
+                                <p class="card-text">Text</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,7 +1,20 @@
 export default function MovieDetails() {
     return (
         <>
-            <h1 className="text-center">Movie Details</h1>
+        <div className="container">
+            <h1 className="text-center mb-5">Movie Details</h1>
+
+            <div className="row">
+                <div class="card p-5">
+                    <img class="card-img-top" src="" alt="Title" />
+                    <div class="card-body">
+                        <h4 class="card-title">Movie details</h4>
+                        <p class="card-text">Text</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
         </>
     )
 }
