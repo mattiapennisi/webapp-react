1ae523e239b276b879cd7db9ad2baf991d5c91d2
mattiapennisi <mattiapennisi0@gmail.com>
2025-04-22 13:04:53 +0200

    Enhance MovieReviewsForm layout with styling and input fields for username and review voting

--- src/components/MovieReviewsForm.jsx
+++ src/components/MovieReviewsForm.jsx
@@ -1,7 +1,17 @@
 export default function MovieReviewsForm() {
     return (
         <>
-            <form>
+            <h2 className="my-5">Add your review</h2>
+            <form className="d-flex gap-3">
+                <input type="text" placeholder="Enter your username" required />
+                <label htmlFor="review-vote">Vote</label>
+                <select name="review-vote" id="review-vote">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
                 <input type="text" placeholder="Enter your review text" required />
                 <button type="submit">Submit</button>
             </form>
